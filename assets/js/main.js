0ad6e8ccab42da36c3e6a6c34fd9979979a7a7a5
Judith <judith.sanmartin.s@gmail.com>
2024-04-08 20:28:29 -0400

    estructura constructor

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 const getData = async () => {
     try {
         const response = await fetch('animales.json');
-        
+
         console.log(response)
         if (response.ok === false) {
             console.log("no encuentro animal")
@@ -11,8 +11,24 @@ const getData = async () => {
             }
         }
         const data = await response.json();
+        console.log(data)
     } catch (error) {
         console.log(error)
     }
 }
 getData()
+
+const tablaRegistro = document.querySelector("#Tabla")
+const nomAnimal = document.querySelector("#animal")
+const añoEdad = document.querySelector("#edad")
+const comentario = document.querySelector("#comentarios")
+
+tablaRegistro.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+    console.log("esta procesando")
+    const tipoAnimal =nomAnimal.Value
+    const tramoEdad = añoEdad.Value
+    const descripcion =comentario.Value
+
+
+})
