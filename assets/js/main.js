35a56621d9908002556f2f3b253eba7a40950419
Judith <judith.sanmartin.s@gmail.com>
2024-04-14 22:12:27 -0400

    iiff

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,71 @@
+class Animal {
+    #name
+    #edad
+    #img
+    #comentarios
+    #sonido
+    constructor(name, edad, img, comentarios, sonido) {
+        this.#name = name;
+        this.#edad = edad;
+        this.#img = img;
+        this.#comentarios = comentarios;
+        this.#sonido = sonido;
+    }
+    get name() {
+        return this.#name
+    }
+    get edad() {
+        return this.#edad
+    }
+    get img() {
+        return this.#img
+    }
+
+    get sonido() {
+        return this.#sonido
+    }
+    get comentario() {
+        return this.#comentarios
+    }
+
+    set comentario(descripcion) {
+        this.#comentarios = descripcion
+    }
+    pintarHTML() {
+        console.log('estoy pintando al personaje')
+    }
+}
+
+class Leon extends Animal {
+    pintarHTML() {
+        console.log(`estoy pintando a ${this.name}`)
+    }
+}
+class Lobo extends Animal {
+    pintarHTML() {
+        console.log(`estoy pintando a ${this.name}`)
+    }
+}
+class Oso extends Animal {
+    pintarHTML() {
+        console.log(`estoy pintando a ${this.name}`)
+    }
+}
+class Serpiente extends Animal {
+    pintarHTML() {
+        console.log(`estoy pintando a ${this.name}`)
+    }
+}
+class Aguila extends Animal {
+    pintarHTML() {
+        console.log(`estoy pintando a ${this.name}`)
+    }
+}
+
 const getData = async () => {
     try {
         const response = await fetch('animales.json');
 
-        console.log(response)
         if (response.ok === false) {
             console.log("no encuentro animal")
             throw {
@@ -16,19 +79,51 @@ const getData = async () => {
         console.log(error)
     }
 }
-getData()
 
-const tablaRegistro = document.querySelector("#Tabla")
-const nomAnimal = document.querySelector("#animal")
-const añoEdad = document.querySelector("#edad")
-const comentario = document.querySelector("#comentarios")
+(async()=> {
+   await getData();
+})()
 
-tablaRegistro.addEventListener("submit", (evento) => {
-    evento.preventDefault()
+const tablaRegistro = document.querySelector("#Tabla");
+const nomAnimal = document.querySelector("#animal");
+const añoEdad = document.querySelector("#edad");
+const comentario = document.querySelector("#comentarios");
+
+tablaRegistro.addEventListener("submit", async (evento) => {
+    evento.preventDefault();
     console.log("esta procesando")
-    const tipoAnimal =nomAnimal.Value
-    const tramoEdad = añoEdad.Value
-    const descripcion =comentario.Value
+    const nomAnimal = nomAnimal.value;
+    const rangoEdad = añoEdad.value;
+    const comentarios = comentario.value;
 
+    if (!nomAnimal  || !rangoEdad) {
+        console.log('Por favor, complete todos los campos');
+        return;
+    }
 
-})
+    switch (nomAnimal) {
+        case "Leon":
+            const animal1 = new Leon("León", rangoEdad, "", comentarios, "Rugido");
+            animal1.pintarHTML();
+            break;
+        case "Lobo":
+            const animal2 = new Lobo("Lobo", rangoEdad, "", comentarios, "Aullar");
+            animal2.pintarHTML();
+            break;
+        case "Oso":
+            const animal3 = new Oso("Oso", rangoEdad, "", comentarios, "Gruñir");
+            animal3.pintarHTML();
+            break;
+        case "Serpiente":
+            const animal4 = new Serpiente("Serpiente", rangoEdad, "", comentarios, "Sisear");
+            animal4.pintarHTML();
+            break;
+        case "Aguila":
+            const animal5 = new Aguila("Aguila", rangoEdad, "", comentarios, "Chillar");
+            animal5.pintarHTML();
+            break;
+        default:
+            console.log("El animal Seleccionado no esta disponible")
+    }
+
+});
