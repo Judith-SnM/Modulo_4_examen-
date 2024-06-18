6d548b62f8db47bc625605b9aba4803b8bc48b44
Judith <judith.sanmartin.s@gmail.com>
2024-06-18 12:09:03 -0400

    terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,32 +37,36 @@ class Animal {
 }
 
 class Leon extends Animal {
-    pintarHTML() {
-        console.log(`estoy pintando a ${this.name}`)
+    constructor(edad, comentarios) {
+        super("León", edad, "assets/img/leon.png", comentarios, "Rugido.mp3");
     }
 }
 class Lobo extends Animal {
-    pintarHTML() {
-        console.log(`estoy pintando a ${this.name}`)
+    constructor(edad, comentarios) {
+        super("Lobo", edad, "assets/img/lobo.jpg", comentarios, "Aullido.mp3");
     }
 }
 class Oso extends Animal {
-    pintarHTML() {
-        console.log(`estoy pintando a ${this.name}`)
+    constructor(edad, comentarios) {
+        super("Oso", edad, "assets/img/oso.jpg", comentarios, "Gruñido.mp3");
     }
 }
 class Serpiente extends Animal {
-    pintarHTML() {
-        console.log(`estoy pintando a ${this.name}`)
+    constructor(edad, comentarios) {
+        super("Serpiente", edad, "assets/img/serpiente.jpg", comentarios,"Siseo.mp3");
     }
 }
 class Aguila extends Animal {
-    pintarHTML() {
-        console.log(`estoy pintando a ${this.name}`)
+    constructor(edad, comentarios) {
+        super("Águila", edad, "assets/img/aguila.png", comentarios, "Chillido.mp3");
     }
 }
 
-const getData = async () => {
+//iniciar animales
+let animales = [];
+
+//IIFE: funcion autoejecutable
+(async () => {
     try {
         const response = await fetch('animales.json');
 
@@ -74,53 +78,74 @@ const getData = async () => {
             }
         }
         const data = await response.json();
-        console.log(data)
+        animales = data
     } catch (error) {
         console.log(error)
     }
-}
-
-(async()=> {
-   await getData();
-})()
+})();
 
 const tablaRegistro = document.querySelector("#Tabla");
 const nomAnimal = document.querySelector("#animal");
 const añoEdad = document.querySelector("#edad");
 const comentario = document.querySelector("#comentarios");
 
-tablaRegistro.addEventListener("submit", async (evento) => {
+tablaRegistro.addEventListener("submit", (evento) => {
     evento.preventDefault();
     console.log("esta procesando")
     const nomAnimal = nomAnimal.value;
     const rangoEdad = añoEdad.value;
     const comentarios = comentario.value;
 
-    if (!nomAnimal  || !rangoEdad) {
+    if (!nomAnimal || !rangoEdad) {
         console.log('Por favor, complete todos los campos');
         return;
     }
 
     switch (nomAnimal) {
         case "Leon":
-            const animal1 = new Leon("León", rangoEdad, "", comentarios, "Rugido");
-            animal1.pintarHTML();
+            const animalEncontradoLeon = animales.find(item => item.name === "Leon");
+            if (animalEncontradoLeon) {
+                const animal1 = new Leon(rangoEdad, comentarios);
+                animal1.pintarHTML();
+            } else {
+                console.log("El animal no está disponible");
+            }
             break;
         case "Lobo":
-            const animal2 = new Lobo("Lobo", rangoEdad, "", comentarios, "Aullar");
-            animal2.pintarHTML();
+            const animalEncontradoLobo = animales.find(item => item.name === "Lobo");
+            if (animalEncontradoLobo) {
+                const animal2 = new Lobo(rangoEdad, comentarios);
+                animal2.pintarHTML();
+            } else {
+                console.log("El animal no está disponible");
+            }
             break;
         case "Oso":
-            const animal3 = new Oso("Oso", rangoEdad, "", comentarios, "Gruñir");
-            animal3.pintarHTML();
+            const animalEncontradoOso = animales.find(item => item.name === "Oso");
+            if (animalEncontradoOso) {
+                const animal3 = new Oso(rangoEdad, comentarios);
+                animal3.pintarHTML();
+            } else {
+                console.log("El animal no está disponible");
+            }
             break;
         case "Serpiente":
-            const animal4 = new Serpiente("Serpiente", rangoEdad, "", comentarios, "Sisear");
-            animal4.pintarHTML();
+            const animalEncontradoSerpiente = animales.find(item => item.name === "Serpiente");
+            if (animalEncontradoSerpiente) {
+                const animal4 = new Serpiente(rangoEdad, comentarios);
+                animal4.pintarHTML();
+            } else {
+                console.log("El animal no está disponible");
+            }
             break;
         case "Aguila":
-            const animal5 = new Aguila("Aguila", rangoEdad, "", comentarios, "Chillar");
-            animal5.pintarHTML();
+            const animalEncontradoAguila = animales.find(item => item.name === "Aguila");
+            if (animalEncontradoAguila) {
+                const animal5 = new Aguila(rangoEdad, comentarios);
+                animal5.pintarHTML();
+            } else {
+                console.log("El animal no está disponible");
+            }
             break;
         default:
             console.log("El animal Seleccionado no esta disponible")
